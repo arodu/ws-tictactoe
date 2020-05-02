58900fa7ca3a69d08c10ab2a50098c212f16babf
Alberto Rodriguez <arodu.dev@gmail.com>
2020-05-02 11:39:53 -0400

    update

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = process.env.PORT || '3000';
 app.set('port', port);
 
 var server = require('http').Server(app);
